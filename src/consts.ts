8a7346ec6f59c84809ce6ccc66bb430408a2d890
stellaakira <stellaakira@outlook.com>
2025-03-06 14:34:41 +0000

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'https://iili.io/2mMpv4t.webp';
+export const SITE_DESCRIPTION = 'Katalogo de Karawan';
---
+++ src/content/categorias/electronica.md
@@ -0,0 +1,8 @@
+---
+title: "electronica"
+description: "Electronica si"
+pubDate: "Jun 19 2024"
+heroImage: "https://iili.io/3JgDRQj.webp"
+---
+# La cucaracha
+La cucaracha, ya no puede caminar, porque le falta, porque le fuckta, la patita de delatra
