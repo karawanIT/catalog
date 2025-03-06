2eba34ad0d2dd3fc183c5dfa3a5c6dda3670a900
stellaakira <stellaakira@outlook.com>
2025-03-06 15:22:14 +0000

    Fixed link third time

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://karawanit.github.io',
-	base: 'catalog',
+	site: 'https://karawanit.github.io/catalog/',
 	integrations: [mdx(), sitemap()],
 });
